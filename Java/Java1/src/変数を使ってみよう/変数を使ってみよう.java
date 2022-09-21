703e7245def367c1f8fb7a37e473332afa124cf6
kawa-sho <101601548+kawa-sho@users.noreply.github.com>
2022-09-21 12:27:37 +0900

    Add Java

---
+++ Java/Java1/src/変数を使ってみよう/変数を使ってみよう.java
@@ -0,0 +1,27 @@
+package 変数を使ってみよう;
+
+public class 変数を使ってみよう {
+	public static void main(String[] args) {
+	    int number1 = 3;
+
+	    // int型の変数number2を定義し、7を代入してください
+	    int number2 = 7;
+
+	    // number1 * number2を出力してください
+	    System.out.println(number1 * number2);
+
+	    // 変数textに「プログラミングを勉強しよう」を代入してください
+	    String text = "プログラミングを勉強しよう";
+
+	    // 「Progateで」と変数textを連結して出力してください
+	    System.out.println("Progateで" + text);
+
+	  }
+}
+
+
+//
+//int型の変数number2を定義し、7を代入してください。
+//number1 * number2 を出力してください。
+//String型の変数textを定義し、プログラミングを勉強しようを代入してください。
+//文字列のProgateでと変数textを連結して出力してください。
