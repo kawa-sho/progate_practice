6be22ed47220582009aefd5e4bacaede1ba6568b
kawa-sho <101601548+kawa-sho@users.noreply.github.com>
2022-10-14 05:49:19 +0900

    Add Java

---
+++ Java/Java1/src/変数を使ってみよう/自己代入.java
@@ -0,0 +1,19 @@
+package 変数を使ってみよう;
+
+public class 自己代入 {
+	public static void main(String[] args) {
+	    int number = 3;
+	    System.out.println(number);
+
+	    // 変数numberの値に7を足して、変数numberを上書きしてください
+	    number += 7;
+
+	    // 変数numberを出力してください
+	    System.out.println(number);
+
+	  }
+}
+
+//変数を上書きします。
+//・「+」を用いて、変数numberの値に7を足して、変数numberを上書きしてください。
+//・再度、変数numberを出力してください。
