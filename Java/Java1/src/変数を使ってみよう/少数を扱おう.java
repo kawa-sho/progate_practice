530ac9daf11e86c490967db51574fcb14002e5d3
kawa-sho <101601548+kawa-sho@users.noreply.github.com>
2022-10-19 13:29:57 +0900

    Add Java

---
+++ Java/Java1/src/変数を使ってみよう/少数を扱おう.java
@@ -0,0 +1,24 @@
+package 変数を使ってみよう;
+
+public class 少数を扱おう {
+	public static void main(String[] args) {
+	    // double型の変数number1を定義し、8.5を代入してください。
+	    double number1 = 8.5;
+
+	    // double型の変数number2を定義し、3.4を代入してください。
+	    double number2 = 3.4;
+
+	    // number1にnumber2を足した値を出力してください
+	    System.out.println(number1+number2);
+
+	    // number1からnumber2を引いた値を出力してください
+	    System.out.println(number1-number2);
+
+	  }
+}
+
+
+//・double型の変数number1を定義し、8.5を代入してください。
+//・double型の変数number2を定義し、3.4を代入してください。
+//number1 + number2 を出力してください。
+//number1 - number2 を出力してください。
