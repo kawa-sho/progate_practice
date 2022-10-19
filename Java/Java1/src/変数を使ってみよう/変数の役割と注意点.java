b4392893d1d56924dd32cab3ce003b9cf13562ef
kawa-sho <101601548+kawa-sho@users.noreply.github.com>
2022-10-19 13:23:35 +0900

    Add Java

---
+++ Java/Java1/src/変数を使ってみよう/変数の役割と注意点.java
@@ -0,0 +1,27 @@
+package 変数を使ってみよう;
+
+public class 変数の役割と注意点 {
+	public static void main(String[] args) {
+	    int length = 6;
+	    int height = 8;
+
+	    // 変数rectangleAreaに、四角形の面積を代入してください
+	    int rectangleArea = length * height;
+
+	    // 変数rectangleAreaを出力してください
+	    System.out.println(rectangleArea);
+
+	    // 変数triangleAreaに、三角形の面積を代入してください
+	    int triangleArea = length * height / 2;
+
+	    // 変数triangleAreaを出力してください
+	    System.out.println(triangleArea);
+
+	  }
+}
+
+
+//・変数rectangleAreaを定義し、length * heightを代入してください（掛け算の式をそのまま変数に代入できます）。
+//・変数rectangleAreaを出力してください。
+//・変数triangleAreaを定義し、length * height / 2を代入してください。
+//・変数triangleAreaを出力してください。
