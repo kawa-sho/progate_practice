35fce3a889cd3ce56e28cecb059bc4df88670177
kawa-sho <101601548+kawa-sho@users.noreply.github.com>
2022-10-24 19:30:36 +0900

    Add Java

---
+++ Java/Java1/src/型変換/自動型変換.java
@@ -0,0 +1,27 @@
+package 型変換;
+
+public class 自動型変換 {
+	public static void main(String[] args) {
+	    int month = 12;
+	    int date = 31;
+
+	    // 「12月31日」となるように変数と文字列を連結して出力してください
+	    System.out.println(month+"月"+date+"日");
+
+	    // 7を2で割った値を出力してください
+	    System.out.println(7/2);
+
+	    // 7.0を2.0で割った値を出力してください
+	    System.out.println(7.0/2.0);
+
+	    // 7を2.0で割った値を出力してください
+	    System.out.println(7/2.0);
+
+	  }
+}
+
+
+//int型の変数monthと変数dateが用意されています。「12月31日」となるように、これらの変数と文字列を連結して出力してください。
+//7を2で割った値を出力してください
+//7を2で割った値を出力してください
+//7を2.0で割った値を出力してください
