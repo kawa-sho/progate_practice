f27b615c573902a7c403d6ae986613e3916112cb
kawa-sho <101601548+kawa-sho@users.noreply.github.com>
2022-10-27 13:26:19 +0900

    Add Java

---
+++ Java/Java2/src/真偽値と条件式/論理演算子.java
@@ -0,0 +1,21 @@
+package 真偽値と条件式;
+
+public class 論理演算子 {
+	public static void main(String[] args) {
+	    // trueと出力されるようにしてください
+	    System.out.println(true || false);
+
+	    // falseと出力されるようにしてください
+	    System.out.println(false && true);
+
+	    // 「8 < 5」かつ「3 >= 2」の結果を出力してください
+	    System.out.println(8<5&&3>=2);
+
+	    // 「8 < 5」または「3 >= 2」の結果を出力してください
+	    System.out.println(8<5||3>=2);
+
+	    // 「8 < 5」でない、の結果を出力してください
+	    System.out.println(!(8<5));
+
+	  }
+}
