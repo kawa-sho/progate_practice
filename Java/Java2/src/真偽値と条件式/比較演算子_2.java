280354d7699fa8cad8cace07f2701acfa242fef9
kawa-sho <101601548+kawa-sho@users.noreply.github.com>
2022-10-27 12:45:06 +0900

    Add Java

---
+++ Java/Java2/src/真偽値と条件式/比較演算子_2.java
@@ -0,0 +1,12 @@
+package 真偽値と条件式;
+
+public class 比較演算子_2 {
+	public static void main(String[] args) {
+	    // 「4 + 2」と「6」を比較し、falseとなるようにしてください
+	    System.out.println(4+2 < 6);
+
+	    // 「4 + 2」と「6」を比較し、trueとなるようにしてください
+	    System.out.println(4+2 <= 6);
+
+	  }
+}
