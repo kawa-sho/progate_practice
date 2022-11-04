8b076c473e5c3e8268e67a577c85fa8558549e54
kawa-sho <101601548+kawa-sho@users.noreply.github.com>
2022-11-04 20:27:42 +0900

    Add Java

---
+++ Java/Java2/src/繰り返し処理/for文.java
@@ -0,0 +1,11 @@
+package 繰り返し処理;
+
+public class for文 {
+	public static void main(String[] args) {
+	    // for文を用いて、繰り返し処理をつくってください
+	    for(int i = 1;i <= 10; i++) {
+	      System.out.println(i+"回目のループです");
+	    }
+
+	  }
+}
