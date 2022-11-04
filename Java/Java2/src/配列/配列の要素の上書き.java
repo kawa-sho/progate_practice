399880cc4ef52bcc315f30fa25334fc564de1e89
kawa-sho <101601548+kawa-sho@users.noreply.github.com>
2022-11-04 23:31:23 +0900

    Add Java

---
+++ Java/Java2/src/配列/配列の要素の上書き.java
@@ -0,0 +1,18 @@
+package 配列;
+
+public class 配列の要素の上書き {
+	public static void main(String[] args) {
+	    // 変数languagesを定義し、配列を代入してください
+	    String[] languages = {"Ruby", "PHP", "Python"};
+
+	    // インデックス番号が「1」の要素を出力してください
+	    System.out.println(languages[1]);
+
+	    // インデックス番号が「1」の要素を「Java」で上書きしてください
+	    languages[1] = "Java";
+
+	    // インデックス番号が「1」の要素を出力してください
+	    System.out.println(languages[1]);
+
+	  }
+}
