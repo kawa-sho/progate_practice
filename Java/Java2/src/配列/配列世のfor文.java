a34da9864e30bd37385c915437f6412df203324a
kawa-sho <101601548+kawa-sho@users.noreply.github.com>
2022-11-04 23:36:40 +0900

    Add Java

---
+++ Java/Java2/src/配列/配列世のfor文.java
@@ -0,0 +1,13 @@
+package 配列;
+
+public class 配列世のfor文 {
+	public static void main(String[] args) {
+	    String[] names = {"にんじゃわんこ", "ひつじ仙人", "ベイビーわんこ"};
+
+	    // 配列用のfor文を用いて、「私の名前は◯◯です」と出力してください
+	    for (String name:names) {
+	      System.out.println("私の名前は"+name+"です");
+	    }
+
+	  }
+}
